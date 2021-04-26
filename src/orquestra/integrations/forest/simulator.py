4ff6c710563bf0ba901e70542bc62cb10ecede37
Alexander Juda <6004040+alexjuda@users.noreply.github.com>
2021-04-26 17:52:51 +0200

    Migrate to new circuits (#25)
    * Migrate run_circuit_and_measure to the new circuits
    
    * Migrate the rest of backend methods to new circuits
    
    * Update docstrings and run black
    
    * Add dev extras for installing pytest
    
    * Run isort
    
    * Remove unused imports
    
    * Fix too long strings
    
    * Make exception class more specific

--- src/orquestra/integrations/forest/simulator.py
+++ src/orquestra/integrations/forest/simulator.py
@@ -1,20 +1,18 @@
-import os
+import errno
+import socket
+import subprocess
 
 import numpy as np
+from pyquil.api import WavefunctionSimulator, get_qc
+from zquantum.core.circuit import Circuit as OldCircuit
 from zquantum.core.interfaces.backend import QuantumSimulator
-from zquantum.core.circuit import save_circuit
 from zquantum.core.measurement import (
-    load_wavefunction,
-    load_expectation_values,
-    sample_from_wavefunction,
     ExpectationValues,
-    expectation_values_to_real,
     Measurements,
 )
 from zquantum.core.openfermion import qubitop_to_pyquilpauli
-from pyquil.api import WavefunctionSimulator, get_qc
-import subprocess
-import socket, errno
+from zquantum.core.wip.circuits import export_to_pyquil, new_circuit_from_old_circuit
+from zquantum.core.wip.compatibility_tools import compatible_with_old_type
 
 
 class ForestSimulator(QuantumSimulator):
@@ -47,13 +45,16 @@ class ForestSimulator(QuantumSimulator):
 
         s.close()
 
+    @compatible_with_old_type(
+        old_type=OldCircuit, translate_old_to_wip=new_circuit_from_old_circuit
+    )
     def run_circuit_and_measure(self, circuit, n_samples=None, **kwargs):
         """Run a circuit and measure a certain number of bitstrings. Note: the number
         of bitstrings measured is derived from self.n_samples
 
         Args:
-            circuit (zquantum.core.circuit.Circuit): the circuit to prepare the state
-            n_samples (int): The number of samples to measure. If None, then the
+            circuit: the circuit to prepare the state
+            n_samples: The number of samples to measure. If None, then the
                 number of samples is taken from the n_samples attribute.
         Returns:
             a list of bitstrings (a list of tuples)
@@ -62,21 +63,24 @@ class ForestSimulator(QuantumSimulator):
             n_samples = self.n_samples
         super().run_circuit_and_measure(circuit)
         cxn = get_forest_connection(self.device_name)
-        bitstrings = cxn.run_and_measure(circuit.to_pyquil(), trials=n_samples)
+        bitstrings = cxn.run_and_measure(export_to_pyquil(circuit), trials=n_samples)
         if isinstance(bitstrings, dict):
             bitstrings = np.vstack([bitstrings[q] for q in sorted(cxn.qubits())]).T
 
-        # Store the bitstrings as a list of tuples, with each tuple representing one bitstring
         bitstrings = [tuple(b) for b in bitstrings.tolist()]
         return Measurements(bitstrings)
 
+    @compatible_with_old_type(
+        old_type=OldCircuit, translate_old_to_wip=new_circuit_from_old_circuit
+    )
     def get_exact_expectation_values(self, circuit, qubit_operator, **kwargs):
         self.number_of_jobs_run += 1
         self.number_of_circuits_run += 1
         if self.device_name != "wavefunction-simulator" or self.n_samples is not None:
-            raise Exception(
-                f"""To compute exact expectation values, (i) the device name must be "wavefunction-simulator" and (ii) n_samples 
-                    must be None. The device name is currently {self.device_name} and n_samples is {self.n_samples}."""
+            raise RuntimeError(
+                "To compute exact expectation values, (i) the device name must be "
+                "\"wavefunction-simulator\" and (ii) n_samples must be None. The device "
+                f"name is currently {self.device_name} and n_samples is {self.n_samples}."
             )
         cxn = get_forest_connection(self.device_name)
 
@@ -86,31 +90,33 @@ class ForestSimulator(QuantumSimulator):
 
         pauli_sum = qubitop_to_pyquilpauli(qubit_operator)
         expectation_values = np.real(
-            cxn.expectation(circuit.to_pyquil(), pauli_sum.terms)
+            cxn.expectation(export_to_pyquil(circuit), pauli_sum.terms)
         )
 
         if expectation_values.shape[0] != len(pauli_sum):
             raise (
                 RuntimeError(
-                    "Expected {} expectation values but received {}.".format(
-                        len(pauli_sum), expectation_values.shape[0]
-                    )
+                    f"Expected {len(pauli_sum)} expectation values but received "
+                    f"{expectation_values.shape[0]}."
                 )
             )
         return ExpectationValues(expectation_values)
 
+    @compatible_with_old_type(
+        old_type=OldCircuit, translate_old_to_wip=new_circuit_from_old_circuit
+    )
     def get_wavefunction(self, circuit):
         super().get_wavefunction(circuit)
         cxn = get_forest_connection(self.device_name)
-        wavefunction = cxn.wavefunction(circuit.to_pyquil())
+        wavefunction = cxn.wavefunction(export_to_pyquil(circuit))
         return wavefunction
 
 
-def get_forest_connection(device_name):
+def get_forest_connection(device_name: str):
     """Get a connection to a forest backend
 
     Args:
-        device_name (string): the device to connect to
+        device_name: the device to connect to
 
     Returns:
         A connection to either a pyquil simulator or a QPU
