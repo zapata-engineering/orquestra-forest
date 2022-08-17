f0d7876f21130e03368363bf70287ce7fad8ee0d
mstechly <michal.stechly@gmail.com>
2022-08-17 12:44:57 -0400

    Add Copyright for ticket: ZQS-1162

--- src/orquestra/integrations/forest/conversions/__init__.py
+++ src/orquestra/integrations/forest/conversions/__init__.py
@@ -1,5 +1,5 @@
 ################################################################################
-# © Copyright 2021 Zapata Computing Inc.
+# © Copyright 2021-2022 Zapata Computing Inc.
 ################################################################################
 from ._circuit_conversions import export_to_pyquil, import_from_pyquil
 from ._pauli_conversions import orq_to_pyquil, pyquil_to_orq
